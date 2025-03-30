9abdd32c655c11e513ba11d795030f52995a6ee5
dasundularaka <dasundularaka@gmail.com>
2025-03-30 23:18:55 +0530

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -15,7 +15,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *SOBIA MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SOBIA MD BY SOBIA BUTT*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *ZERO XMD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©ZERO XMD BY DASUN DULARAKA*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`*Error Fetching Anime Girl image*: ${e.message}`);
